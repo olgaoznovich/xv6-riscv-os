a9099467d29da2eb8bb3cda0a9629826e7ee6db8
ol-ozn <ol.oznovich@gmail.com>
2024-05-28 16:06:38 +0000

    task2

--- Makefile
+++ Makefile
@@ -119,6 +119,7 @@ UPROGS=\
 	$U/_cat\
 	$U/_echo\
 	$U/_helloworld\
+	$U/_memsize_test\
 	$U/_forktest\
 	$U/_grep\
 	$U/_init\
---
+++ user/memsize_test.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    printf("memsize before allocation: %d\n", memsize());
+    void* allocation = malloc(20000);
+    printf("memsize after allocation: %d\n", memsize());
+    free(allocation);
+    printf("memsize after freeing: %d\n", memsize());
+
+    exit(0);
+}
